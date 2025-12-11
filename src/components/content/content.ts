2954da9a8da3c7447cda2290ca983f66729c4b87
Marc <spurdo.dk@gmail.com>
2025-12-11 14:22:25 +0100

    update

--- src/components/content/content.ts
+++ src/components/content/content.ts
@@ -3,7 +3,7 @@ import "./content.css";
 export function renderContent(host: HTMLDivElement): void {
   host.innerHTML = `
   
-  <button class="button button-primary xs-full-width">
+  <button class="button button-secondary xs-full-width">
       Opret scenario
   </button>
 
